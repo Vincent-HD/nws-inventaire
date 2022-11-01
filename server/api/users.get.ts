c2ce910d610fe3bd4e7282403f702eca8608bc16
Vincent <vincenthoudan@gmail.com>
2022-11-01 01:56:36 +0000

    Fix ending lines linux + add dev container

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default defineEventHandler(async event => {
+    return "Vasy allo ?"
     const result = await prisma.user.findMany()
     return result
 })
